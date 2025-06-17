80a170338cfc2357075e7672d37841724ef2be85
T-dot-prog <tahasinahoni2@gmail.com>
2025-06-17 20:11:35 +0600

    committed test functionality

---
+++ demo.py
@@ -0,0 +1,17 @@
+# from src.logger import logger
+# from src.exception import MyException
+# import sys
+
+# logger.info("this is an info message")
+# logger.warning("this is an warning message")
+
+# try:
+#     result = 10 / 0 
+# except Exception as e:
+#     logger.error("there is a error encountering")
+#     raise MyException(error_message= e, error_detail= sys) from e 
+
+
+# all test passed successfully -----------------------------
+
+
